25beb4c123bd06b81320aac16fd07a3027437305
Hajar Althuhli <157451006+Hajar-45@users.noreply.github.com>
2024-09-30 12:29:11 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('result');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += ` ${operator} `;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
